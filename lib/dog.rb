31278b8b8256e503c58af15ea53e74f2dbf990e6
Ella Sklar <ella.soccer22@gmail.com>
2018-07-13 18:54:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,23 +4,9 @@ class Dog
   
   attr_accessor :name , :breed 
 
-def name=(name) 
-  @name = name
-end 
-
-def name 
-  @name 
-end 
-
-def breed=(breed)
-  @breed = breed
-end 
-
-def breed 
-  @breed
-end 
   
-
-
+fido = Dog.new
+puts fido.name = "Fido"
+puts fido.breed = "Husky"
 
 end 
